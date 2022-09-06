27fb2dee08ab708f462aaaad86db5a44b7014d7d
MateusMo <mat.marq.oliveira@gmail.com>
2022-09-06 07:35:56 -0300

    Criando ApiKey

--- Configuration.cs
+++ Configuration.cs
@@ -2,7 +2,9 @@
 {
     public static class Configuration
     {
-        public static string JwtKey { get; set; } = "ZkVkERo98FD9cxFREKLHN98fdSFDW";
+        public static string JwtKey = "ZkVkERo98FD9cxFREKLHN98fdSFDW";
+        public static string ApiKeyName = "api_key";
+        public static string ApiKey = "curso_api99043509ÇgjkfdjkgjF%dkj&fdKSDJ";
 
     }
 }
